dd2dd29e47aa518738d8587936ef89580d76cf1f
FranciscoLi2000 <francisco2000li@icloud.com>
2025-02-07 19:04:30 +0000

    libadd

--- includes/libft.h
+++ includes/libft.h
@@ -48,4 +48,26 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 /* 分配足够空间并复制参数 s 指向的字符串，返回新字符串的指针。如果分配失败，则返回 NULL。 */
 char	*ft_strdup(const char *s);
 
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+
+typedef struct	s_list
+{
+	void		*content;
+	struct s_list	*next;
+}			t_list;
+
+
+
+
+
 #endif
