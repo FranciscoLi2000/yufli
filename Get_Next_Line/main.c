fd5acca8d9b6e734cb866a3f16a611946ec1925b
FranciscoLi2000 <francisco2000li@icloud.com>
2025-02-13 21:18:20 +0000

    GNL0213

--- Get_Next_Line/main.c
+++ Get_Next_Line/main.c
@@ -6,29 +6,4 @@
 
 int	main(int argc, char **argv)
 {
-	int	fd;
-	char	*line;
-
-	if (argc != 2)
-	{
-		printf("Usage: %s <filename>\n", argv[0]);
-		return (1);
-	}
-	fd = open(argv[1], O_RDONLY);
-	if (fd < 0)
-	{
-		perror("Error opening file");
-		return (1);
-	}
-	line = get_next_line(fd);
-	if (!line)
-	{
-		printf("Error: get_next_line returned NULL\n");
-		close(fd);
-		return (1);
-	}
-	while (line != NULL)
-		printf("%s", line);
-	close(fd);
-	return (0);
 }
